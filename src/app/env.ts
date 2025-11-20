ef6ec28fdd410d9632961d128b162c3ca35f9707
Johnny <funnyvalentineofi@gmail.com>
2025-11-20 16:22:47 -0500

    update

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,4 +1,4 @@
 export const ENV = {
-  // API_URL: 'http://localhost:3000/api/'
-  API_URL : 'https://backend-lacus.onrender.com/api/',
+  API_URL: 'http://localhost:3000/api/'
+  // API_URL : 'https://backend-lacus.onrender.com/api/',
 };
